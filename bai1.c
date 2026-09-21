5c511a7c15cc49e548fa0b48cdccfd45f093b53d
buiquynh0868-crypto <buiquynh0868@gmail.com>
2026-09-22 02:04:39 +0700

    Create bai1.c
    bài tập KTLT bai L1.1

---
+++ bai1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main () { 
+  printf ("****************************************\n");
+  printf ("***   HOC VIEN HANG KHONG VIET NAM   ***\n"); 
+  printf ("***          KHOA DIEN - DIEN TU     ***\n");
+   printf ("***       NGON NGU LAP TRINH C      ***\n");  
+  printf ("****************************************\n");
+	return 0;  
+}
+	
